4b45e741915932f7fb2f990892028e20665f0590
ALLEN <aljay3334@gmail.com>
2021-10-23 11:36:29 +0100

    Checking prime number alternative

--- prime.js
+++ prime.js
@@ -14,3 +14,36 @@ function isPrime(number) {
 
 console.log(isPrime(8)); // Output is false
 console.log(isPrime(3)); //Output is true
+
+//Alternative
+
+function isPrime2(number) {
+  let isPrime = true;
+
+  //Check if number is equal to 1
+  if (number === 1) {
+    console.log("1 is neither prime nor composite number");
+  }
+
+  //check if number is greater than 1
+  else if (number > 1) {
+    //looping through 2 to number-1
+    for (let i = 2; i < number; i++) {
+      if (number % i == 0) {
+        isPrime = false;
+        break;
+      }
+    }
+    if (isPrime) {
+      console.log(`${number} is a prime number`);
+    } else {
+      console.log(`${number} is a not prime number`);
+    }
+  }
+  //Check if number is less than 1
+  else {
+    console.log(`The number is not a prime number`);
+  }
+}
+
+isPrime2(4);
