cf7d336d1d9f3613617fd0abfee496526bfbb7a4
ALLEN <aljay3334@gmail.com>
2021-11-03 12:29:59 +0100

    TwoSum array problem solution2 optimized solution

--- arrayProblems/TwoSum/solution.js
+++ arrayProblems/TwoSum/solution.js
@@ -12,3 +12,23 @@ const twoSum1 = (array, goal) => {
     }
     return indexes;
 }
+
+
+//0(n) time complexity 
+const twoSum2 = (array, goal) => {
+    let mapOfNumbers = {};
+    let twoIndexes = [];
+
+    for (let i = 0; i < array.length; i++) {
+        mapOfNumbers[array[i]] = i
+    }
+
+    for (let i = 0; i < array.length; i++) {
+        let target = goal - arr[i];
+        if (mapOfNumbers[target] !== null && mapOfNumbers[target] !== i) {
+            twoIndexes.push(i);
+            twoIndexes.push(mapOfNumbers[target]);
+        }
+    }
+    return twoIndexes;
+}
