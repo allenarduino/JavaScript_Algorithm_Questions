29d1c912b9bf1435452ea62c651d95b1c0479899
ALLEN <aljay3334@gmail.com>
2021-11-03 12:21:32 +0100

    Array problems solution 1

--- arrayProblems/TwoSum/problem.md
+++ arrayProblems/TwoSum/problem.md
@@ -8,7 +8,7 @@ and you may not use the same element twice.
 
 Example:
 
-Given nums = [2, 7, 11, 15], target = 9,
+Given array = [2, 7, 11, 15], target = 9,
 
 Because nums[0] + nums[1] = 2 + 7 = 9,
 return [0, 1].
--- arrayProblems/TwoSum/solution.js
+++ arrayProblems/TwoSum/solution.js
@@ -1,3 +1,14 @@
-const twoSum = (array, goal) => {
+//O(n^2) time complexity
+const twoSum1 = (array, goal) => {
+    let indexes = [];
 
+    for (let i = 0; i < array.length; i++) {
+        for (let j = i + 1; j < array.length; j++) {
+            if (arry[i] + array[j] === goal) {
+                indexes.push(i);
+                indexes.push(j);
+            }
+        }
+    }
+    return indexes;
 }
