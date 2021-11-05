66661a8f2671fc3490942843b0d669f4f7ad6755
ALLEN <aljay3334@gmail.com>
2021-11-05 15:34:33 +0100

    Js function to find prime factors of a number

---
+++ primeFactors.js
@@ -0,0 +1,18 @@
+//Function to find prime factors of a number
+
+function primeFactors(n) {
+  var factors = [];
+  divisor = 2;
+
+  while (n > 2) {
+    if (n % divisor == 0) {
+      factors.push(divisor);
+      n = n / divisor;
+    } else {
+      divisor++;
+    }
+    return factors;
+  }
+}
+
+console.log(primeFactors(8));
